4c40fa5108e566ef1dd50f26057d03e57ef264ca
Ganesh Bhandarkar <gyb142000@gmail.com>
2021-04-15 15:40:30 +0530

    Time: 0 ms (100.00%), Space: 6 MB (35.91%) - LeetHub

---
+++ fibonacci-number/fibonacci-number.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int fib(int n) {
+        int dp[31] = {0};
+        dp[0] = 0;
+        dp[1]=1;
+        dp[2]=1;
+        for(int i=3;i<31;i++){
+            dp[i]=dp[i-1]+dp[i-2];
+        }
+        return dp[n];
+    }
+};
